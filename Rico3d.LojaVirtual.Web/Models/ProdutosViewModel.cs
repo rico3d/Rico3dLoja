e17006ff598280e5be916b64a173dfaedf219c97
rico3d <rico3d2001@gmail.com>
2016-03-11 10:37:08 -0300

    Teste Identidade

---
+++ Rico3d.LojaVirtual.Web/Models/ProdutosViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ASP.NET_MVC5_Bootstrap_3._3._4_LESS1.Dominio.Entidades;
+
+namespace ASP.NET_MVC5_Bootstrap_3._3._4_LESS1.Models
+{
+    public class ProdutosViewModel
+    {
+        
+
+        public IEnumerable<Produto> Produtos { get; set; }
+        public Paginacao Paginacao { get; set; }
+
+        public string CategoriaAtual { get; set; }
+
+    }
+}
